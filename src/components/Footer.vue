5dd4a21f8adf37a0f969b6d1febbb2aa444f8f5c
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-04-25 15:30:05 -0300

    adicionando rotas

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Estacionamento app 2022 - Coffee tag</p>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="cadastro">
+    <h1>Cadastro de carros</h1>
+  </div>
+</template>
