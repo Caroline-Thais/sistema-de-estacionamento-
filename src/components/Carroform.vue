c068ae8acab0f8fc2c6d409e20eda2a1699d6b39
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-04-26 10:22:44 -0300

    adicionando formulário na view Home

---
+++ src/components/Carroform.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div id="carro-form">
+            <div class="input-container">
+                <label for="name">Nome do cliente:</label>
+                <input type="text" id="nome" v-model="nome" placeholder="nome">
+            </div>
+
+            <div class="input-container">
+                <label for="carro">Carro:</label>
+                <input type="text" id="carro" v-model="carro" placeholder="carro">
+            </div>
+
+            <div class="input-container">
+                <label for="placa">Placa:</label>
+                <input type="text" id="placa" v-model="placa" placeholder="placa">
+            </div>
+
+            <div class="input-container">
+                <label for="hora">Horário de entrada:</label>
+                <input type="text" id="hora" v-model="hora" placeholder="hora entrada">
+            </div>
+
+           <div class="input-container">
+                
+                <input type="submit" class="submit-btn" value="Cadastrar">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//import { defineComponent } from '@vue/composition-api'
+
+export default {
+  name: "Carroform"
+}
+</script>
+
+<style scoped>
+#carro-form{
+    max-width: 300px;
+    margin: 0 auto;
+}
+
+
+.input-container{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+
+label{
+    font-weight: bold;
+    margin-bottom: 15px;
+    padding: 5px 10px;
+    border-left: 4px solid greenyellow;
+}
+
+input{
+    padding: 5px 10px;
+    width: 300px;
+}
+
+.submit-btn{
+    background-color: aquamarine;
+    color: gray;
+    padding: 10px;
+    cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="main-container">
    <h1>Cadastrar veículos</h1>
+   <Carroform/>
   </div>
 </template>
 
 <script>
+import Carroform from '../components/Carroform.vue'
 export default {
+  components:{
+  Carroform
+  }
 }
 </script>
 
